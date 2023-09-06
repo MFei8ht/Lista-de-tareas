3ad1ffc4b606ac7025a653cfd32906c92b353ecb
Octavio Becerril Olivares <136529745+MFei8ht@users.noreply.github.com>
2023-09-05 19:21:57 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,18 @@
+
+package listatareas;
+public class ListaTareas {
+    public static void main(String[] args) {
+        String tarea1=("hacer apuntes");
+        String tarea2=("comer");
+        String tarea3=("estudiar");
+        String tarea4=("dormirte un rato");
+        String tarea5=("acomodar tu cuarto"); 
+        System.out.println("***LISTA DE TAREAS***");
+        System.out.println("TAREA 1: "+tarea1);
+        System.out.println("TAREA 2: "+tarea2);
+        System.out.println("TAREA 3: "+tarea3);
+        System.out.println("TAREA 4: "+tarea4);
+        System.out.println("TAREA 5: "+tarea5);
+    }
+    
+}
